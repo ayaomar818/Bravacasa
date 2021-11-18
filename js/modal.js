4e07443e51a4b6bedecfd403348101492ad601bc
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-18 10:58:50 +0200

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,130 @@
+
+    function openModal() {
+      document.getElementById("Modal").style.display = "block";
+    }
+    function openmyModal() {
+      document.getElementById("myModal").style.display = "block";
+    }
+    function openmyModal_a() {
+      document.getElementById("myModal_a").style.display = "block";
+    }
+    function openmyModal_b() {
+      document.getElementById("myModal_b").style.display = "block";
+    }
+    function openModal_a() {
+      document.getElementById("Modal_a").style.display = "block";
+    }
+    function openModal_b() {
+      document.getElementById("Modal_b").style.display = "block";
+    }
+    function openModal_c() {
+      document.getElementById("Modal_c").style.display = "block";
+    }
+    function openModal_d() {
+      document.getElementById("Modal_d").style.display = "block";
+    }
+    function openModal_e() {
+      document.getElementById("Modal_e").style.display = "block";
+    }
+    function openModal_f() {
+      document.getElementById("Modal_f").style.display = "block";
+    }
+    function openModal_g() {
+      document.getElementById("Modal_g").style.display = "block";
+    }
+    function openModal_h() {
+      document.getElementById("Modal_h").style.display = "block";
+    }
+    function openModal_k() {
+      document.getElementById("Modal_k").style.display = "block";
+    }
+    function openModal_m() {
+      document.getElementById("Modal_m").style.display = "block";
+    }
+    function openModal_n() {
+      document.getElementById("Modal_n").style.display = "block";
+    }
+    function openModal_p() {
+      document.getElementById("Modal_p").style.display = "block";
+    }
+    function openModal_q() {
+      document.getElementById("Modal_q").style.display = "block";
+    }
+    function openModal_r() {
+      document.getElementById("Modal_r").style.display = "block";
+    }
+    function openModal_s() {
+      document.getElementById("Modal_s").style.display = "block";
+    }
+    function closeModal() {
+      document.getElementById("Modal").style.display = "none";
+      
+    }
+    function closemyModal() {
+      document.getElementById("myModal").style.display = "none";
+      
+    }
+    function closemyModal_a() {
+      document.getElementById("myModal_a").style.display = "none";
+    
+    }
+    function closemyModal_b() {
+      document.getElementById("myModal_b").style.display = "none";
+      
+    }
+    function closeModal_a() {
+        document.getElementById("Modal_a").style.display = "none";
+      
+    }
+    function closeModal_b() {
+        document.getElementById("Modal_b").style.display = "none";
+    }
+    
+    function closeModal_c() {
+        document.getElementById("Modal_c").style.display = "none";
+    }
+    
+    function closeModal_d() {
+        document.getElementById("Modal_d").style.display = "none";
+    }
+    function closeModal_e() {
+        document.getElementById("Modal_e").style.display = "none";
+    }
+    function closeModal_f() {
+        document.getElementById("Modal_f").style.display = "none";
+    }
+    function closeModal_g() {
+        document.getElementById("Modal_g").style.display = "none";
+    }
+    function closeModal_h() {
+        document.getElementById("Modal_h").style.display = "none";
+    }
+    function closeModal_k() {
+        document.getElementById("Modal_k").style.display = "none";
+    }
+    function closeModal_m() {
+        document.getElementById("Modal_m").style.display = "none";
+    }
+    function closeModal_n() {
+        document.getElementById("Modal_n").style.display = "none";
+    }
+    function closeModal_p() {
+        document.getElementById("Modal_p").style.display = "none";
+    }
+    function closeModal_q() {
+        document.getElementById("Modal_q").style.display = "none";
+    }
+    function closeModal_r() {
+        document.getElementById("Modal_r").style.display = "none";
+    }
+    function closeModal_s() {
+        document.getElementById("Modal_s").style.display = "none";
+    }
+    
+      
+      
+      
+      
+     
+     
+ 
