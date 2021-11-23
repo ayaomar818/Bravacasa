12396244bc40cd67a111a8146cd32dd7b5b1e34d
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-23 15:17:30 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,16 @@
+<?php
+$name= $_POST ['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = '';
+$email_subject = "New ";
+$email_body = "Name: $name.\n".
+              "Email: $visitor_email.\n".
+              "Message: $message.\n";
+$to = "@gmail.com";
+$headers = "From : $email_from \r\n";
+$headers .="Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header ("Location: index.html");
+?>
