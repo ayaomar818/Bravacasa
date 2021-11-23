64c0dc4ae9ce2be5c7f3ffb1c75a7e69caefda1a
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-23 17:27:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,36 +35,3 @@ AOS.init({
     easing: 'ease-out-back',
     duration: 3000
 });
-
-window.addEventListener("DOMContentLoader", function() {
-    var form =document.getElementById("form");
-    var status =document.getElementById("status");
-    function success (){
-        form.reset();
-        status.classList.add('success');
-        status.innerHTML="Send Successfuly";
-    }
-    function error(){
-        status.classList.add('error');
-        status.innerHTML="something get rong. please try again";
-    }
-    form.addEventListener("submit",function(ev) {
-        ev.preventDefault();
-        var data =new FormData(form);
-        ajax(form.method, form.action , data , success, error);
-    });
-});
-function ajax (method , url , data , success , error){
-    var xhr = new XMLHttpRequest();
-    xhr.open(method,url);
-    xhr.setRequestHeader("Accept","application/json");
-    xhr.onreadystatechange=function() {
-        if(xhr.readyState !== XMLHttpRequest.DONE) return;
-        if (xhr.status === 200) {
-            success(xhr.response, xhr.responseType);
-        } else {
-            error (xhr.status , xhr.response , xhr.responseType);
-        }
-    };
-    xhr.send(data);
-}
