bf7dfc42650a8702e810faf56c7752e03afa7059
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-23 15:19:07 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@ $email_subject = "New ";
 $email_body = "Name: $name.\n".
               "Email: $visitor_email.\n".
               "Message: $message.\n";
-$to = "@gmail.com";
+$to = "ayaomar818@gmail.com";
 $headers = "From : $email_from \r\n";
 $headers .="Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
