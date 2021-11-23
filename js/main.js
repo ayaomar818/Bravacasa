2206e08e28b99e7d60ee3b485b180020c61ee73b
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-23 17:17:34 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ $(function () {
 })
 
 
-
 // When the user scrolls down 50px from the top of the document, resize the navbar's size
 window.onscroll = function () { scrollFunction() };
 
@@ -30,7 +29,42 @@ function scrollFunction() {
     }
 }
 
+
+// Aos animation
 AOS.init({
     easing: 'ease-out-back',
     duration: 3000
 });
+
+window.addEventListener("DOMContentLoader", function() {
+    var form =document.getElementById("form");
+    var status =document.getElementById("status");
+    function success (){
+        form.reset();
+        status.classList.add('success');
+        status.innerHTML="Send Successfuly";
+    }
+    function error(){
+        status.classList.add('error');
+        status.innerHTML="something get rong. please try again";
+    }
+    form.addEventListener("submit",function(ev) {
+        ev.preventDefault();
+        var data =new FormData(form);
+        ajax(form.method, form.action , data , success, error);
+    });
+});
+function ajax (method , url , data , success , error){
+    var xhr = new XMLHttpRequest();
+    xhr.open(method,url);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onreadystatechange=function() {
+        if(xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error (xhr.status , xhr.response , xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
