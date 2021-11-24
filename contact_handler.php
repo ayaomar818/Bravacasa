f6609ecbaf96ff305b1d2446179ad073dd1dbc72
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-24 11:33:45 +0200

    Add files via upload

--- contact_handler.php
+++ contact_handler.php
@@ -15,17 +15,25 @@
 // header ("Location: index.html");
 if(isset($_POST['Send'])){
     $to = "ayaomar818@gmail.com"; // this is your Email address
-    $from = $_POST['Email']; // this is the sender's Email address
     $name = $_POST['Name'];
+    $from = $_POST['Email']; // this is the sender's Email address
     $subject = "Form submission";
+    $subject2 = "Confirmation: Message was submitted successfully "; // For customer confirmation
     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['Message'];
-
+    $message2 = "Dear" . $name . "\n"
+                . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+                . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+                . "Regards," ;
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    
+    $result1= mail($to,$subject,$message,$headers);
+    $result2= mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    // echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    if ($result1 && $result2) {
+        $success = "Your Message was sent Successfully!";
+      } else {
+        $failed = "Sorry! Message was not sent, Try again Later.";
     }
+}
 
 ?>
