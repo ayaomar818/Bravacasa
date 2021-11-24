7f202a610f5f10458a348d31d1fa5cbdf1d02bb6
ayaomar818 <90695695+ayaomar818@users.noreply.github.com>
2021-11-24 15:17:47 +0200

    Add files via upload

--- contact_handler.php
+++ contact_handler.php
@@ -1,46 +1,11 @@
 <?php
-// $name= $_POST ['Name'];
-// $visitor_email = $_POST['Email'];
-// $message = $_POST['Message'];
-
-// $email_from = 'https://ayaomar818.github.io/Bravacasa/';
-// $email_subject = "New ";
-// $email_body = "Name: $name.\n".
-//               "Email: $visitor_email.\n".
-//               "Message: $message.\n";
-// $to = "ayaomar818@gmail.com";
-// $headers = "From : $email_from \r\n";
-// $headers .="Reply-To: $visitor_email \r\n";
-// mail($to,$email_subject,$email_body,$headers);
-// header ("Location: index.html");
-// if(isset($_POST['Send'])){
-//     $to = "ayaomar818@gmail.com"; // this is your Email address
-//     $name = $_POST['Name'];
-//     $from = $_POST['Email']; // this is the sender's Email address
-//     $subject = "Form submission";
-//     $subject2 = "Confirmation: Message was submitted successfully "; // For customer confirmation
-//     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['Message'];
-//     $message2 = "Dear" . $name . "\n"
-//                 . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
-//                 . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
-//                 . "Regards," ;
-//     $headers = "From:" . $from;
-//     $headers2 = "From:" . $to;
-//     $result1= mail($to,$subject,$message,$headers);
-//     $result2= mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-//     // echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-//     if ($result1 && $result2) {
-//         $success = "Your Message was sent Successfully!";
-//       } else {
-//         $failed = "Sorry! Message was not sent, Try again Later.";
-//     }
-// }
 if(isset($_POST['Name']) && isset($_POST['Email'])){
-    $name = $_POST['Name'];
-    $email = $_POST['Email'];
+    $name = $_POST['Name'];  
+    $email = $_POST['Email'];   // this is your Email address
 	$message = $_POST['Message'];
-    $to = 'ayaomar818@gmail.com';
-    $subject = "From BRAVACASA website";
+    $to = '';
+    $subject = "Message From BRAVACASA website";
+    $subject2 = "Confirmation: Message was send successfully "; // For customer confirmation
     $body='<html>
     <body> 
     <h2> feedback </h2>
@@ -49,30 +14,32 @@ if(isset($_POST['Name']) && isset($_POST['Email'])){
 	<p>Message<br>'.$message.'</p>
     </body>
     </html>';
-
+    $message2 = "Dear" . $name . "\n"
+                . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
+                . "You send the following message: " . "\n" . $_POST['Message'] . "\n\n"
+                . "Regards," ;
     //headers
     $headers = "From: ".$name." <".$email.">\r\n";
     $headers .="Reply-To: ".$email."\r\n";
 	$headers .="MIME-Version:1.0\r\n";
 	$headers .="Content-type:text/html;charset=utf-8";
-
+    $headers2 = "From:" . $to;
     $send = mail($to, $subject, $body, $headers);
-    if($send){
+    $send2 = mail($email, $subject2, $message2, $headers2);
+    if($send && $send2){
     echo'<html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" >
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        
-        </head><body> <div class="form-container">
+        </head><body> <div style="text-align: center; margin-top: 20px;">
 		<div id="success_message">
-                    <h3>Your message sent successfully!</h3> 
-                    <p> We will get back to you soon. </p>
-                </div>
-				</div></body></html>';
+            <h3>Your message sent successfully!</h3> 
+            <p> We will get back to you soon. </p>
+        </div>
+        </div></body></html>';
     }else{
 		echo'<html><head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" >
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        
-        </head><body><div class="form-container"><div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div></div></body><html>';
+        </head><body><div style="text-align: center; margin-top: 20px;"><div id="error_message" style="width:100%; height:100%; "> <h3>Error</h3>  Sorry there was an error sending your message. please try again.
+        </div></div></body><html>';
     }
-
 }
 ?>
--- join_handler.php
+++ join_handler.php
@@ -1,46 +1,30 @@
 <?php
-// if(isset($_POST['submit'])){
-// $name= $_POST ['Name'];
-// $email = $_POST['Email'];
-// $address = $_POST['Address'];
-// $mobile = $_POST['Mobile'];
-// $file = $_POST['CV'];
-
-// $email_subject = "New CV";
-// $email_body = "Name: $name.\n".
-//               "Email: $email.\n".
-//               "Address: $address.\n".
-//               "Mobile: $mobile.\n". 
-//               "CV : $file.\n";
-// $to = "ayaomar818@gmail.com";
-// $headers = "From : $email_from \r\n";
-// $headers .="Reply-To: $email \r\n";
-// mail($to,$email_subject,$email_body,$headers);
-// echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-// header ("Location: index.html");
-// $success = '<div class="alert alert-success">Send Successfuly</div>'
-// }
 if(isset($_POST['submit'])){
-    $to = "ayaomar818@gmail.com"; // this is your Email address
+    $to = ""; // this is the company's Email address
     $name = $_POST['Name'];
     $from = $_POST['Email']; // this is the sender's Email address
     $address = $_POST['Address'];
     $mobile = $_POST['Mobile'];
     $file = $_POST['CV'];
-    $subject = "Form submission";
+    $subject = "CV From BRAVACASA website";
     $subject2 = "Confirmation: Message was submitted successfully "; // For customer confirmation
+    $email_body = "Name: $name.\n".
+              "Email: $email.\n".
+              "Address: $address.\n".
+              "Mobile: $mobile.\n". 
+              "CV : $file.\n";
     $message2 = "Dear" . $name . "\n"
-                . "Thank you for contacting us. We will get back to you shortly!" . "\n\n"
-                . "Regards," ;
+                . "Thank you for contacting us. We will contact you as soon as possible" . "\n\n"
+                . "Best Regards,". "\n\n" . "BRAVACASA" ;
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    $result1= mail($to,$subject,$headers);
+    $result1= mail($to,$subject,$email_body,$headers);
     $result2= mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    // echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
     if ($result1 && $result2) {
-        $success = "Your Message was sent Successfully!";
-      } else {
-        $failed = "Sorry! Message was not sent, Try again Later.";
+        // $success = '<div class="alert alert-success">Send Successfuly</div>'
+        echo "Mail Sent. Thank you " . $name ;
+    } else {
+        echo "Sorry! Message was not sent, Try again Later.";
     }
 }
 ?>
